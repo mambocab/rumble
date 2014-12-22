ac43f5e8e19586890007564ec65dc13948d53ffc
Jim Witschey <jim.witschey@gmail.com>
2014-12-22 01:12:22 -0500

    adds tests for report module

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,4 +1,33 @@
+import pytest
+
+from simpletimeit.datatypes import Report, TimedFunction
 from simpletimeit import report
+try:
+    from unittest.mock import Mock, patch
+except ImportError:
+    from mock import Mock, patch
+
+
+@pytest.fixture
+def default_reporter():
+    results = Report(best=1, number=1000, repeat=3,
+                     timedfunction=TimedFunction(function=Mock(name='one'),
+                                                 group='jets',
+                                                 args='range(100)'))
+    results = Report(best=2, number=1000, repeat=3,
+                     timedfunction=TimedFunction(function=Mock(name='two'),
+                                                 group='sharks',
+                                                 args='range(100)'))
+    results = Report(best=3, number=1000, repeat=3,
+                     timedfunction=TimedFunction(function=Mock(name='three'),
+                                                 group='sharks',
+                                                 args='range(1000)'))
+    return report.DefaultTableGenerator(results)
+
+def test_title_empty_group_name(default_reporter):
+    a = 'range(100)'
+    result = default_reporter.render_title_for('', a)
+    assert result == 'args: {}'.format(a)
 
 
 def test_title_empty_group_name():
