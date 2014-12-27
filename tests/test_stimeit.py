9fe4501958a727dc8cffaa48b250ad8bd152f5d7
Jim Witschey <jim.witschey@gmail.com>
2014-12-27 16:02:11 -0500

    fixes test assertion for python 2

--- tests/test_stimeit.py
+++ tests/test_stimeit.py
@@ -46,9 +46,6 @@ def test_call_with_setup():
 def test_call_with_invalid_input():
     st = stimeit.SimpleTimeIt()
     with pytest.raises(ValueError):
-        # repr(map(...)) doesn't look like declaration of that map
-        st.call_with(map(str, (1, 2, 4, 8, 16)))
-    with pytest.raises(ValueError):
         st.call_with(lambda x: None)
 
 
