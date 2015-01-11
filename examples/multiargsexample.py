9e0774aa36ab1bb45a51404a1044b291e38221f0
Jim Witschey <jim.witschey@gmail.com>
2015-01-11 14:46:20 -0500

    fixes final name in multiargs example

--- examples/multiargsexample.py
+++ examples/multiargsexample.py
@@ -6,7 +6,7 @@ r = rumble.Rumble()
 
 r.arguments(10, 100, _name='easy: 10 and 100')
 r.arguments(8000, 92833, _name='medium: 8000 and 92833')
-r.arguments(898989, 1000000000001, _name='898989, 1000000000001')
+r.arguments(898989, 1000000000001, _name='hard: 898989 and 1000000000001')
 
 @r.contender
 def divide(a, b):
